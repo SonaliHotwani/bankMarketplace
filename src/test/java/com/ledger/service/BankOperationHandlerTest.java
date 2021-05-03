ea16b409a85175cc4ae9edaadfa18be4de114e85
sonalih5 <sonalih@thoughtworks.com>
2021-05-03 10:54:52 +0530

    #6 | Sonali | Adds test in handler for invalid operation case

--- src/test/java/com/ledger/service/BankOperationHandlerTest.java
+++ src/test/java/com/ledger/service/BankOperationHandlerTest.java
@@ -1,12 +1,11 @@
 package com.ledger.service;
 
-import com.ledger.model.BalanceOperation;
-import com.ledger.model.BankUser;
-import com.ledger.model.LoanOperation;
-import com.ledger.model.PaymentOperation;
+import com.ledger.exception.InvalidBankOperationException;
+import com.ledger.model.*;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import static org.mockito.Mockito.*;
@@ -40,5 +39,21 @@ class BankOperationHandlerTest {
         verify(loanOperation).update(handler.getBankStateFor(bankUser));
         verify(paymentOperation).update(handler.getBankStateFor(bankUser));
         verify(balanceOperation).update(handler.getBankStateFor(bankUser));
+    }
+
+    @Test
+    void shouldNotUpdateBankStateForInvalidBankOperation() {
+        final BankOperationTransformer transformer = mock(BankOperationTransformer.class);
+        final BankOperationHandler handler = new BankOperationHandler(transformer);
+        List<String> bankOperationsString = Collections.singletonList(
+                "VIEW IDIDI Dale 5000 1 6");
+        final BankOperation invalidOperation = mock(BankOperation.class);
+        final BankUser bankUser = new BankUser("IDIDI", "Dale");
+
+        final InvalidBankOperationException exception = new InvalidBankOperationException("Operation: " + bankOperationsString.get(0) + " not supported");
+        when(transformer.getBankOperation("VIEW IDIDI Dale 5000 1 6")).thenThrow(exception);
+
+        handler.handle(bankOperationsString);
+        verify(invalidOperation, times(0)).update(any());
     }
 }
